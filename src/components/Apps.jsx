79781f1f2561075b6ccd5f86f5a401f637154cd9
Akash Sarker <akashsarker2478@gmail.com>
2025-10-08 03:14:01 +0600

    search box add

--- src/components/Apps.jsx
+++ src/components/Apps.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useAppsData from './Hook/useAppsData';
 import ProductCard from './ProductCard';
 
 const Apps = () => {
     const {products} = useAppsData()
+    const [search,setSearch] = useState('')
+    const term = search.trim().toLocaleLowerCase()
+    const searchedProducts = term? products.filter(product=>product.title.toLocaleLowerCase().includes(term)) : products
+    console.log(searchedProducts)
     return (
         <div>
-            <div>
+            <div className='flex justify-between'>
                 <h2 className='font-bold'>({products.length})  Apps Found</h2>
+                <label className="input">
+ 
+  <input 
+  value = {search}
+  onChange={(e)=>setSearch(e.target.value)} type="search" placeholder="search apps" />
+</label>
             </div>
              <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-3 my-5'>
              {
-               products.map(product=><ProductCard key={product.id} product={product}></ProductCard>)
+               searchedProducts.map(product=><ProductCard key={product.id} product={product}></ProductCard>)
             }
            </div>
         </div>
